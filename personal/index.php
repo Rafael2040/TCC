0a857fa1a0637d32bc5f378f5632f0bc8ba0f233
Rafael Fornari <rafaelzfornari@gmail.com>
2020-07-31 14:30:20 -0300

    modificacoes

--- personal/index.php
+++ personal/index.php
@@ -1,8 +1,17 @@
 <?php
 session_start();
-include '../utils/verifica_sessao.php';
-?>
 
+require '../utils/verifica_sessao.php';
+require '../bd/conexao.php';
+
+$personal_id = $_SESSION['id'];
+$personal_nome = $_SESSION['nome'];
+
+//buscar alunos no banco
+$sql = "SELECT * FROM aluno";
+$alunos = mysqli_query($conexao, $sql) or die($conexao->close());
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,7 +19,13 @@ include '../utils/verifica_sessao.php';
 </head>
 <body>
 	<h1>Bem vindo <?=$_SESSION['nome']?></h1>
+	<h2><a href="./aluno_formulario.php">Adicionar Aluno</h2>
 	<h2><a href="logout.php">Sair</a></h2>
+	<ul>
+	<?php while ($aluno = $alunos->fetch_assoc()): ?>
+		<li><a href="#"><?=$aluno['nome']?></a></li>
+	<?php endwhile ?>
+	</ul>
 </body>
 </html>
 
